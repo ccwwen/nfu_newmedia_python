3980965a8a1c701ee6824fa45697f0e85427b137
陈丽媛 <chenliyuan1960@outlook.com>
2017-06-19 21:09:20 +0800

    Update Compendium_of_Materia_Medica_4web.py

--- Compendium_of_Materia_Medica_4web/Compendium_of_Materia_Medica_4web.py
+++ Compendium_of_Materia_Medica_4web/Compendium_of_Materia_Medica_4web.py
@@ -1,17 +1,25 @@
  # -*- coding: utf-8 -*- 
 from flask import Flask, render_template, request, escape
+from pick_medica_value import medica_value
 
 app = Flask(__name__)
 
 
-@app.route('/choose_a_medica', methods=['POST'])
+@app.route('/pick_a_medica', methods=['POST'])
 def pick_a_medica() -> 'html':
-    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    medica_name = request.form['medica_name']	
-    return render_template('results.html',
-                           the_title = '请输入药名：',
-                           the_medica = medica_name 
-                           )
+    try:
+        medica_name = request.form['medica_name']
+        list_medica = medica_value(medica_name)
+        return render_template('results.html',
+                               the_title = '以下是输出结果',
+                               output_value_释名 = list_medica[0],
+                               output_value_气味 = list_medica[1],
+                               output_value_主治 = list_medica[2]
+                               )
+    except:
+        return render_template('entry.html',
+                           the_output_prompt='输入错误',
+                           the_title='欢迎来到本草纲目')
                           
  
     
@@ -22,9 +30,10 @@ def pick_a_medica() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    """Display this webapp's HTML form."""
+   output_prompt = '请输入你想要搜索的药名'
     return render_template('entry.html',
+                           the_output_prompt=output_prompt,
                            the_title='欢迎来到本草纲目')
-
+  
 if __name__ == '__main__':
     app.run(debug=True)
