1410723111f3147f0c1dfca5088136e2bcbf035c
陈丽媛 <chenliyuan1960@outlook.com>
2017-06-21 16:28:38 +0800

    增加
    增加期末专题展示要求的内容

---
+++ Compendium_of_Materia_Medica_4web/webapp_pick_a_medica.py
@@ -0,0 +1,33 @@
+ # -*- coding: utf-8 -*- 
+from flask import Flask, render_template, request, escape
+from pick_medica_value import medica_value
+app = Flask(__name__)
+
+@app.route('/pick_a_medica', methods=['POST'])
+def pick_a_medica() -> 'html':
+    try:
+        medica_name = request.form['medica_name']
+        list_medica = medica_value(medica_name)
+        return render_template('results.html',
+                               the_title = '以下是输出结果',
+                               output_value_释名 = list_medica[0],
+                               output_value_气味 = list_medica[1],
+                               output_value_主治 = list_medica[2]
+                               )
+    except:
+        return render_template('entry.html',
+                           the_output_prompt='出错啦',
+                           the_title='欢迎来到本草纲目')
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    output_prompt = '请输入你想要搜索的药名'
+    return render_template('entry.html',
+                           the_output_prompt=output_prompt,
+                           the_title='欢迎来到本草纲目')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
